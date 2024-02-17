baec5c1aecf27de9ccd969494464311ab7023dda
Rodrigo Moraes <rodrigo.smoraes98@gmail.com>
2024-02-17 16:21:08 -0300

    Minor corrections

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='scistringparser',
-    version='0.1.2',
+    version='0.1.3',
     description='A python module that converts strings in the metric scientific notation to numerical data types.',
     long_description=open("README.md", 'r').read(),
     long_description_content_type='text/markdown',
     author='Rodrigo Moraes',
     author_email='rodrigo.smoraes98@gmail.com',
-    packages=find_packages(),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/RodrigoMoraes98/PyScientificStringParser',
     classifiers=[
         "Programming Language :: Python :: 3",
         'License :: OSI Approved :: MIT License',
